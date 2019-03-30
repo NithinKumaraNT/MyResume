782c5f88c955fc58459ba28747db6c6eb958f331
nt.nithinkumara@gmail.com <nt.nithinkumara@gmail.com>
2019-03-30 16:30:57 +0100

    First working version

--- js/form-submission-handler.js
+++ js/form-submission-handler.js
@@ -93,7 +93,7 @@
             }
             var thankYouMessage = form.querySelector(".thankyou_message");
             if (thankYouMessage) {
-              thankYouMessage.style.display = "block";
+              thankYouMessage.style.display = "inline";
             }
             return;
         };
